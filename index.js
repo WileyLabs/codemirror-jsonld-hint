17ba63f9481aa1a3f584a866c47c797709239806
BigBlueHat <byoung@bigbluehat.com>
2017-02-08 15:14:48 -0500

    Changed the nature of the beast; Understanding++

--- index.js
+++ index.js
@@ -13,47 +13,67 @@
     mod(CodeMirror);
 })(function(CodeMirror) {
 
-  CodeMirror.registerHelper("hint", "jsonld", function(cm, pred) {
-    var terms = [
-      '@base',
-      '@container', '@context',
-      '@graph',
-      '@id',
-      '@language', '@list',
-      '@reverse',
-      '@set',
-      '@type',
-      '@vocab'
-    ];
-    setTimeout(function() {
-      if (!cm.state.completionActive) {
-        cm.showHint({
-          completeSingle: false,
-          hint: function (cm, options) {
-            var cur = cm.getCursor(),
-                token = cm.getTokenAt(cur);
-            var start = token.start - 1,
-                end = token.end;
-            var list = terms;
-            if (token.type === 'property') {
-              var search = token.string.match(/[@]?\w+/);
-              if (search !== null) {
-                list = terms.filter(function(t) {
-                  if (t.indexOf(search) > -1) {
-                    return t;
-                  }
-                });
-              }
-              return {
-                list: list,
-                from: CodeMirror.Pos(cur.line, start+2),
-                to: CodeMirror.Pos(cur.line, end)
-              };
-            }
+  function getText(completion) {
+    if (typeof completion == "string") return completion;
+    else return completion.text;
+  }
+
+  function customHint(cm, data, completion) {
+    // cursor position & token before replacing
+    var cur = cm.getCursor();
+    var token = cm.getTokenAt(cur);
+    // TODO: make suffix configurable?
+    var suffix = '": ""';
+    // range to replace with selected completion
+    var from = completion.from || data.from;
+    var to = completion.to || data.to;
+    var text = getText(completion);
+    var jump_back = 0;
+    // don't add suffix if we're inside of a finished property string (`""`)
+    if (token.string.length > 1 && token.string[token.string.length-1] !== '"') {
+      // move the cursor back by one--inside the value
+      // replace with completion + suffix
+      text += suffix;
+      jump_back = 1;
+    } else {
+      text += '"';
+    }
+    cm.replaceRange(text, from, to, "complete");
+    cm.setCursor(cm.getCursor().line, cm.getCursor().ch - jump_back);
+    CodeMirror.signal(data, "pick", completion);
+  }
+
+  var terms = [
+    '@base',
+    '@container', '@context',
+    '@graph',
+    '@id',
+    '@language', '@list',
+    '@reverse',
+    '@set',
+    '@type',
+    '@vocab'
+  ];
+  CodeMirror.registerHelper("hint", "jsonld", function(cm, options) {
+    var cur = cm.getCursor(),
+        token = cm.getTokenAt(cur);
+    var start = token.start - 1,
+        end = token.end;
+    var list = terms;
+    if (token.type === 'property') {
+      var search = token.string.match(/[@]?\w+/);
+      if (search !== null) {
+        list = terms.filter(function(t) {
+          if (t.indexOf(search) > -1) {
+            return t;
           }
         });
       }
-    }, 100);
-    return CodeMirror.Pass;
+      return {
+        list: list.map(function(item) { return {text: item, hint: customHint}}),
+        from: CodeMirror.Pos(cur.line, start+2),
+        to: CodeMirror.Pos(cur.line, end)
+      };
+    }
   });
 });
